fc8814172211262fc5478d63f8467f5b08d9fa34
benburkhart1 <benburkhart1@46d4e040-02db-11df-adb7-1be7f7e3f636>
2010-08-19 20:59:10 +0000

    Bump to 0.0.3, quick kill all non B&N, better filter list.

--- trunk/nookTaskManager/src/com/nookdevs/taskmanager/NookTaskManager.java
+++ trunk/nookTaskManager/src/com/nookdevs/taskmanager/NookTaskManager.java
@@ -25,9 +25,12 @@ import android.app.ActivityManager.RunningAppProcessInfo;
 import android.view.KeyEvent;
 import android.view.inputmethod.InputMethodManager;
 
+import android.content.Context;
 import android.widget.Button;
 import android.view.LayoutInflater;
 import android.widget.TextView;
+import android.widget.EditText;
+
 import android.widget.ListView;
 import android.widget.AbsListView;
 import android.widget.RelativeLayout;
@@ -53,7 +56,13 @@ public class NookTaskManager extends nookBaseActivity {
 				"com.android.phone", "com.android.inputmethod.latin", "system", "com.itc.bravo.radiopm", 
 				"com.Bravo.UMSServer", "com.bravo.app.screensaver", "com.bravo.ecm.service", 
 				"com.bravo.firmwareupdate", "com.bravo.sync:content", "com.bravo.sync", "com.bravo.wifi",
-				"com.bravo.ereader.activities", "com.bravo.home"
+				"com.bravo.ereader.activities", "com.bravo.home",
+        "com.bravo.thedaily.Daily", "com.bravo.library.LibraryActivity", "com.bravo.store.StoreFrontActivity",
+        "com.bravo.ereader.activities.ReaderActivity", "com.bravo.app.settings.SettingsActivity",
+        "com.bravo.app.settings.wifi.WifiActivity",
+        "com.nookdevs.launcher.LauncherSettings", "com.bravo.home.HomeActivity",
+        "com.nookdevs.launcher.LauncherSelector", "com.bravo.chess.ChessActivity",
+        "com.bravo.sudoku.SudokuActivity", "com.bravo.app.browser.BrowserActivity"
 			};
 
 		PackageManager m_PM;
@@ -68,14 +77,16 @@ public class NookTaskManager extends nookBaseActivity {
         boolean handled = false;
         switch (keyCode) {
             case NOOK_PAGE_UP_KEY_LEFT:
-            case NOOK_PAGE_UP_KEY_RIGHT:
-							m_Mode = "normal";	
+							m_Mode = "all";
 							listProcesses();
 							handled = true;
-              break;
+							break;
+            case NOOK_PAGE_UP_KEY_RIGHT:
+							// Kill all non B&N
+							killAll();
+							break;
             case NOOK_PAGE_DOWN_KEY_LEFT:
-            case NOOK_PAGE_DOWN_KEY_RIGHT:
-							m_Mode = "all";
+							m_Mode = "normal";	
 							listProcesses();
 							handled = true;
               break;
@@ -114,32 +125,11 @@ public class NookTaskManager extends nookBaseActivity {
 			}
 		};
 
-		public void listProcesses()
+		public void killAll()
 		{
-				m_MemoryInfo      = new MemoryInfo();
-				m_ActivityManager.getMemoryInfo( m_MemoryInfo );
-
-				TextView tv = (TextView) findViewById(R.id.availmem);
-				tv.setText(Long.toString(m_MemoryInfo.availMem));
-
-				tv = (TextView) findViewById(R.id.lowmem);
-				tv.setText((m_MemoryInfo.lowMemory == true) ? "Yes" : "No");
-
-				tv = (TextView) findViewById(R.id.threshold);
-				tv.setText(Long.toString(m_MemoryInfo.threshold));
-
 				m_Activities = m_ActivityManager.getRunningAppProcesses();
-				m_Content.removeAllViews();
-
-				String[] names = new String[m_Activities.size()];
 				RunningAppProcessInfo RAPI;
-
-				ImageButton button;
-				TextView text;
-				TextView title1;
 				String importance;
-
-				int cnt = 0;
 
 				for (int i = 0; i < m_Activities.size(); i++)
 				{
@@ -167,59 +157,146 @@ public class NookTaskManager extends nookBaseActivity {
 							break;
 					}
 
-					if (m_Mode.equals("normal"))
+					if (RAPI.importance == RunningAppProcessInfo.IMPORTANCE_SERVICE)
+						continue;
+
+					if (RAPI.importance == RunningAppProcessInfo.IMPORTANCE_EMPTY)
+						continue;
+
+					boolean filtered = false;
+
+					for (int j = 0; j < m_Filter.length; j++)
 					{
-						if (RAPI.importance == RunningAppProcessInfo.IMPORTANCE_SERVICE)
-							continue;
-
-						if (RAPI.importance == RunningAppProcessInfo.IMPORTANCE_EMPTY)
-							continue;
-
-						boolean filtered = false;
-
-						for (int j = 0; j < m_Filter.length; j++)
+						if (RAPI.processName.equals(m_Filter[j]))
 						{
-							if (RAPI.processName.equals(m_Filter[j]))
-							{
-								filtered = true;
-								break;
-							}
+							filtered = true;
+							break;
 						}
-
-						if (filtered == true)
-							continue;
 					}
-		
-					final LayoutInflater inflater = getLayoutInflater();
-					RelativeLayout appdetails = (RelativeLayout) inflater.inflate(R.layout.addapp, m_Content, false);
 
-					button = (ImageButton) appdetails.findViewById(R.id.process);
-					text = (TextView) appdetails.findViewById(R.id.desc);
-					title1 = (TextView) appdetails.findViewById(R.id.title);
+					if (filtered == true)
+						continue;
 
-					text.setText(importance);
-					title1.setText(RAPI.processName);
+					// Not yet
+					if (RAPI.processName.equals("com.nookdevs.taskmanager"))
+						continue;
 
-					button = (ImageButton) appdetails.findViewById(R.id.process);
+					m_ActivityManager.restartPackage(RAPI.processName);
 
-					// We want to use their actual icon if it's available
-					try
-					{
-						button.setImageDrawable(m_PM.getApplicationIcon(RAPI.processName));
-					}
-					catch(Exception ex) 
-					{
-						button.setImageResource(R.drawable.icon);
-					}
-				
-					button.setTag(RAPI);
-					button.setOnClickListener(appKiller);
-					
-					names[cnt] = RAPI.processName;
-					Log.i( LOGTAG, " name is " + RAPI.processName );
-					m_Content.addView(appdetails, cnt);
-					cnt++;
+					Log.v( LOGTAG, "Killed " + RAPI.processName + " in killAll()");
 				}
+
+				// Suicide
+				m_ActivityManager.restartPackage("com.nookdevs.taskmanager");
+		}
+
+
+		public void listProcesses()
+		{
+			m_MemoryInfo      = new MemoryInfo();
+			m_ActivityManager.getMemoryInfo( m_MemoryInfo );
+
+			TextView tv = (TextView) findViewById(R.id.availmem);
+			tv.setText(Long.toString(m_MemoryInfo.availMem));
+
+			tv = (TextView) findViewById(R.id.lowmem);
+			tv.setText((m_MemoryInfo.lowMemory == true) ? "Yes" : "No");
+
+			tv = (TextView) findViewById(R.id.threshold);
+			tv.setText(Long.toString(m_MemoryInfo.threshold));
+
+			m_Activities = m_ActivityManager.getRunningAppProcesses();
+			m_Content.removeAllViews();
+
+			String[] names = new String[m_Activities.size()];
+			RunningAppProcessInfo RAPI;
+
+			ImageButton button;
+			TextView text;
+			TextView title1;
+			String importance;
+
+			int cnt = 0;
+
+			for (int i = 0; i < m_Activities.size(); i++)
+			{
+				RAPI = m_Activities.get(i);
+
+				switch (RAPI.importance)
+				{
+					case RunningAppProcessInfo.IMPORTANCE_BACKGROUND:
+						importance = "Background";
+						break;
+					case RunningAppProcessInfo.IMPORTANCE_FOREGROUND:
+						importance = "Foreground";
+						break;
+					case RunningAppProcessInfo.IMPORTANCE_SERVICE:
+						importance = "Service";
+						break;
+					case RunningAppProcessInfo.IMPORTANCE_VISIBLE:
+						importance = "Visible";
+						break;
+					case RunningAppProcessInfo.IMPORTANCE_EMPTY:
+						importance = "";
+						break;
+					default:
+						importance = "";
+						break;
+				}
+
+				if (m_Mode.equals("normal"))
+				{
+					if (RAPI.importance == RunningAppProcessInfo.IMPORTANCE_SERVICE)
+						continue;
+
+					if (RAPI.importance == RunningAppProcessInfo.IMPORTANCE_EMPTY)
+						continue;
+
+					boolean filtered = false;
+
+					for (int j = 0; j < m_Filter.length; j++)
+					{
+						if (RAPI.processName.equals(m_Filter[j]))
+						{
+							filtered = true;
+							break;
+						}
+					}
+
+					if (filtered == true)
+						continue;
+				}
+
+				final LayoutInflater inflater = getLayoutInflater();
+				RelativeLayout appdetails = (RelativeLayout) inflater.inflate(R.layout.addapp, m_Content, false);
+
+				button = (ImageButton) appdetails.findViewById(R.id.process);
+				text = (TextView) appdetails.findViewById(R.id.desc);
+				title1 = (TextView) appdetails.findViewById(R.id.title);
+
+				text.setText(importance);
+				title1.setText(RAPI.processName);
+
+				button = (ImageButton) appdetails.findViewById(R.id.process);
+
+				// We want to use their actual icon if it's available
+				try
+				{
+					button.setImageDrawable(m_PM.getApplicationIcon(RAPI.processName));
+				}
+				catch(Exception ex) 
+				{
+					button.setImageResource(R.drawable.icon);
+				}
+
+				button.setTag(RAPI);
+				button.setOnClickListener(appKiller);
+
+				names[cnt] = RAPI.processName;
+				Log.v( LOGTAG, " name is " + RAPI.processName );
+				m_Content.addView(appdetails, cnt);
+				cnt++;
+			}
 		}
 
     /** Called when the activity is first created. */
@@ -228,8 +305,10 @@ public class NookTaskManager extends nookBaseActivity {
         LOGTAG = "nookTaskManager";
         NAME = getString(R.string.app_name);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        m_Content = (LinearLayout) findViewById(R.id.processcontainer);
+				setContentView(R.layout.main);
+				m_Content = (LinearLayout) findViewById(R.id.processcontainer);
+
+
 				m_PM = getPackageManager();
     }
 
