06d58c99983feaa50d617e707bc9790808015605
kevin.vajk@gmail.com <kevin.vajk@gmail.com@46d4e040-02db-11df-adb7-1be7f7e3f636>
2010-10-04 06:30:54 +0000

    Added a copyright message, plus one minor bugfix on the settins screen.

--- trunk/nookMines/src/com/nookdevs/mines/FlashEinkScreenActivity.java
+++ trunk/nookMines/src/com/nookdevs/mines/FlashEinkScreenActivity.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2010 nookDevs
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Written by Kevin Vajk
+ */
+
+
 package com.nookdevs.mines;
 
 //  This activity does nothing.  But it does draw the screen a solid
